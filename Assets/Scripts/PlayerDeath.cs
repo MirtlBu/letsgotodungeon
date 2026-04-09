1f14338eb6f4a0f9a6d93d3c846aaf8e3fc0ee06
mirtlbu <bubuzoglo@gmail.com>
2026-04-09 21:55:21 +0300

    respawn near campfire

--- Assets/Scripts/PlayerDeath.cs
+++ Assets/Scripts/PlayerDeath.cs
@@ -1,17 +1,53 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerDeath : MonoBehaviour
 {
-    [SerializeField] private string respawnPortalId = "overworld-dungeon-exit";
+    [SerializeField] private string respawnSceneName = "Overworld";
+    [SerializeField] private string respawnTargetName = "campfire";
+
+    private HealthSystem health;
 
     void Start()
     {
-        GetComponent<HealthSystem>()?.OnDeath.AddListener(OnDeath);
+        health = GetComponent<HealthSystem>();
+        health?.OnDeath.AddListener(OnDeath);
     }
 
     private void OnDeath()
     {
-        GetComponent<HealthSystem>()?.Heal(float.MaxValue);
-        PortalManager.Instance.Travel("Overworld", respawnPortalId);
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        SceneTransition.Instance.GoToScene(respawnSceneName);
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.name != respawnSceneName) return;
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+        StartCoroutine(RespawnNextFrame());
+    }
+
+    private IEnumerator RespawnNextFrame()
+    {
+        yield return null; // ждём Start() всех объектов сцены
+
+        health?.Heal(float.MaxValue);
+
+        GameObject target = GameObject.Find(respawnTargetName);
+        if (target != null)
+        {
+            var cc = GetComponent<CharacterController>();
+            if (cc != null) cc.enabled = false;
+            transform.position = target.transform.position + new Vector3(2f, 0.5f, 0f);
+            if (cc != null) cc.enabled = true;
+        }
+        else
+        {
+            Debug.LogWarning($"[PlayerDeath] GameObject '{respawnTargetName}' не найден в сцене '{respawnSceneName}'");
+        }
+
+        GetComponent<CharacterMovement>()?.ForceUnground();
+        FindObjectOfType<CameraController>()?.SnapToTarget();
     }
 }
